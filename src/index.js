331fdb62c8823ee51a541c035b15e7716a998046
Mantas Kukulskis <8mantas8@gmail.com>
2025-05-26 19:20:21 +0300

    cookies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import { pageRouter } from './router/pageRouter.js';
 import { apiRouter } from './router/apiRouter.js';
+import { PORT } from './env.js';
+import { cookieParser } from './middleware/cookieParser.js';
+import { getUserData } from './middleware/getUserData.js';
 
 const app = express();
+
+app.use(cookieParser);
+app.use(getUserData);
 
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/', apiRouter);
 app.use('/', pageRouter);
 
-app.listen(5412, () => {
-    console.log(`Serveris: http://localhost:5412`);
+app.listen(PORT, () => {
+    console.log(`Serveris: http://localhost:${PORT}`);
 });
