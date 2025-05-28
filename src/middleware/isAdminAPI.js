71e0fd11d8107afccd125de0ee4649fdf4529c9e
Mantas Kukulskis <8mantas8@gmail.com>
2025-05-28 19:47:32 +0300

    adminAPI

--- src/middleware/isAdminAPI.js
+++ src/middleware/isAdminAPI.js
@@ -1,9 +1,12 @@
-import { Page401 } from "../pages/public/Page401.js";
-
-export async function isAdmin(req, res, next) {
+export function isAdminAPI(req, res, next) {
     if (req.user.role === 'admin') {
         return next();
     }
 
-    return res.send(await (new Page401(req)).render());
+    return res
+        .status(401)
+        .json({
+            status: 'error',
+            msg: 'Unauthorized'
+        });
 }
