955056210f875debd2fb6428437feed38b44528a
Mantas Kukulskis <8mantas8@gmail.com>
2025-06-10 15:48:59 +0300

    Final

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,78 +1,79 @@
-import { connection } from "../../db.js";
-import { getAllCategories } from "../../db/public/categories.js";
-import { IsValid } from "../../lib/IsValid.js";
+const moviesDOM = document.getElementById('movies');
+const formDOM = document.forms[0];
+const textInputDOM = formDOM.elements[0];
+const genreSelectDOM = formDOM.elements[1];
+const durationSelectDOM = formDOM.elements[2];
+const thumbnailDOM = formDOM.elements[3];
 
-export async function apiPublicMoviesGet(req, res) {
-    const availableCategoryIds = (await getAllCategories()).map(c => '' + c.id);
+let timer = null;
 
-    const [err, msg] = IsValid.requiredFields(
-        req.query,
-        [],
-        [
-            { field: 'text', validation: IsValid.nonEmptyString },
-            { field: 'genre', validation: IsValid.includesInList, options: availableCategoryIds },
-            { field: 'duration', validation: IsValid.includesInList, options: ['0', '1', '2', '3', '4'] },
-            { field: 'thumbnail', validation: IsValid.includesInList, options: ['true', 'false'] },
-        ],
-    );
-
-    if (err) {
-        return res.json({
-            status: 'error',
-            msg: msg,
-        });
+function formatMovieDuration(duration) {
+    if (duration < 60) {
+        return `${duration} mins`;
     }
 
-    const text = req.query.text;
-    const genre = +req.query.genre;
-    const duration = +req.query.duration;
-    const thumbnail = req.query.thumbnail === 'true' ? true : false;
-
-    const sqlParts = [];
-    const sqlData = [];
-
-    if (text) {
-        sqlParts.push(`(movies.title LIKE CONCAT("%", ?, "%") OR movies.description LIKE CONCAT("%", ?, "%"))`);
-        sqlData.push(text, text);
-    }
-
-    if (genre) {
-        sqlParts.push('movies.category_id = ?');
-        sqlData.push(genre);
-    }
-
-    if (duration) {
-        sqlParts.push('movies.duration >= ?');
-        sqlData.push((duration - 1) * 60);
-
-        if (duration !== 4) {
-            sqlParts.push('movies.duration <= ?');
-            sqlData.push(duration * 60 - 1);
-        }
-    }
-
-    if (thumbnail) {
-        sqlParts.push('movies.thumbnail != ""');
-    }
-
-    try {
-        const sql = `
-            SELECT movies.*, categories.name AS categoryName, categories.url_slug AS categoryUrlSlug
-            FROM movies
-            INNER JOIN categories ON movies.category_id = categories.id
-            ${sqlParts.length ? 'WHERE ' + sqlParts.join(' AND ') : ''};`;
-        const [result] = await connection.execute(sql, sqlData);
-
-        return res.json({
-            status: 'success',
-            content: result,
-        });
-    } catch (error) {
-        console.log(error);
-
-        return res.json({
-            status: 'error',
-            msg: 'Serverio klaida, nepavyko gauti filmu duomenu',
-        });
-    }
+    return `${Math.floor(duration / 60)} h ${duration % 60} mins`;
 }
+
+function renderCards(data) {
+    let HTML = '';
+
+    for (const item of data) {
+        const img = item.thumbnail ? `/img/movie-thumbnails/${item.thumbnail}` : '/img/default.webp';
+
+        HTML += `
+            <div class="col">
+                <div class="card shadow-sm">
+                    <img src="${img}" class="movie-card-thumbnail card-img-top" style="height: 225px;">
+                    <div class="badge bg-primary movie-card-badge">${item.categoryName}</div>
+                    <div class="card-body">
+                        <a href="/movies/${item.url_slug}" class="h4">${item.title}</a>
+                        <p class="card-text">${item.description}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/movies/${item.url_slug}" class="btn btn-sm btn-outline-secondary">Read more</a>
+                            </div>
+                            <small class="text-body-secondary">${formatMovieDuration(item.duration)}</small>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+    }
+
+    moviesDOM.innerHTML = HTML;
+}
+
+function getData() {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+        const data = {};
+
+        if (textInputDOM.value.trim()) {
+            data.text = textInputDOM.value.trim();
+        }
+        if (genreSelectDOM.value !== '0') {
+            data.genre = genreSelectDOM.value;
+        }
+        if (durationSelectDOM.value !== '0') {
+            data.duration = durationSelectDOM.value;
+        }
+        if (thumbnailDOM.checked === true) {
+            data.thumbnail = true;
+        }
+
+        fetch('/api/movies?' + (new URLSearchParams(data).toString()))
+            .then(res => res.json())
+            .then(data => {
+                if (data.status === 'success') {
+                    renderCards(data.content);
+                }
+            })
+            .catch(console.error);
+    }, 300);
+}
+
+textInputDOM.addEventListener('keyup', getData);
+genreSelectDOM.addEventListener('change', getData);
+durationSelectDOM.addEventListener('change', getData);
+thumbnailDOM.addEventListener('change', getData);
